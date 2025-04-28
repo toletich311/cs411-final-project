3927a7ad966ac4ed7543cda476000bae0add8bbb
toletich311 <toletich@bu.edu>
2025-04-27 20:07:37 -0400

    small intialization shelf model

---
+++ tests/test_shelf_model.py
@@ -0,0 +1,10 @@
+# maybe create class for shelves
+class ShelfManager:
+    def __init__(self):
+        self.shelves = {
+            "want_to_read": [],
+            "currently_reading": [],
+            "finished": []
+        }
+    
+    # maybe add methods for adding, moving, deleting books 
